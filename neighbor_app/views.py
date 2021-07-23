b122717fed03dc532e72510efab0ea72959f3df3
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-23 18:57:03 +0300

    Add activation view

--- neighbor_app/views.py
+++ neighbor_app/views.py
@@ -41,3 +41,28 @@ def signup_view(request):
   else:
     form = SignUpForm()
   return render(request, 'signup.html', {'form': form})
+
+def index(request):
+  return render(request, 'index.html')
+
+def activation_sent_view(request):
+  return render(request, 'registration/activation_sent.html')
+
+
+def activate(request, uidb64, token):
+  try:
+    uid = force_text(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=uid)
+  except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+    user = None
+  # checking if the user exists, if the token is valid.
+  if user is not None and account_activation_token.check_token(user, token):
+    # if valid set active true 
+    user.is_active = True
+    # set signup_confirmation true
+    user.profile.signup_confirmation = True
+    user.save()
+    login(request, user)
+    return redirect('home')
+  else:
+    return render(request, 'registration/activation_invalid.html')
