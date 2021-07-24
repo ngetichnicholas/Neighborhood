e7116e99b98cf8c6a7a564c549bd4d1877b93b5b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-24 10:18:09 +0300

    Update profile class model

--- neighbor_app/models.py
+++ neighbor_app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.deletion import SET_NULL
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+
+
+# Create your models here.
+class NeighborHood(models.Model):
+  pass
 
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,6 +16,10 @@ class Profile(models.Model):
   last_name = models.CharField(max_length=100, blank=True)
   email = models.EmailField(max_length=150)
   signup_confirmation = models.BooleanField(default=False)
+  bio =models.TextField(null=True)
+  profile_picture =CloudinaryField('image')
+  neighborhood = models.ForeignKey(NeighborHood, on_delete=SET_NULL,null=True, related_name='members', blank=True)
+  location =models.CharField(max_length=60,blank=True,null=True)
 
   def __str__(self):
     return self.user.username
