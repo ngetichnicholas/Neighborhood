597d2a3e6d4e8bbef8701bb67f435ffee3f73f36
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-24 12:04:51 +0300

    Remove mistake in neighbor class fields

--- neighbor_app/admin.py
+++ neighbor_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Business, NeighborHood, Post, Profile
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(NeighborHood)
 
